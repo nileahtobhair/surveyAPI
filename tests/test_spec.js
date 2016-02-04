af8a5ae239ebb9fc40d3e7004bef748353671e5d
Niamh Lawlor <nileahtobhair.github.io>
2016-02-04 02:08:19 +0000

    updating test case

--- tests/test_spec.js
+++ tests/test_spec.js
@@ -1,5 +1,16 @@
 var frisby = require('frisby');
 
+//Testing the application home -GET
+frisby.create("/api endpoint testing")
+.get('https://survey-api.herokuapp.com/api')
+.after(function(err, res, body) {
+        console.log("Testing the api basic path");
+    })
+.expectStatus(200)
+.expectJSONLength(1)
+.toss();
+
+
 //Testing the /surveys endpoint -GET
 frisby.create("/surveys endpoint testing")
 .get('https://survey-api.herokuapp.com/api/surveys')
@@ -7,7 +18,7 @@ frisby.create("/surveys endpoint testing")
         console.log("Testing the /surveys endpoint \n1)Expect status -200\n2)Expect 13 surveys to be returned");
     })
 .expectStatus(200)
-.expectJSONLength(12)
+.expectJSONLength(15)
 .toss();
 
 
@@ -15,7 +26,6 @@ frisby.create("/surveys endpoint testing")
 frisby.create("/surveys/find endpoint testing - GET")
 .get('https://survey-api.herokuapp.com/api/surveys/find?subject=technology')
 .afterJSON(function(json) {
-
         console.log("\nTesting the /surveys/find endpoint with subject=technology parameters\n1)Expect status -200 \n2)Expect 5 surveys to be returned");
     })
 .expectStatus(200)
@@ -59,20 +69,52 @@ frisby.create("/surveys/:survey_id endpoint testing - GET")
 
 //Testing the surveys/find endpoint -POST
 frisby.create('Testing the surveys/find endpoint -POST - which uses post data to filter survey results')
-  .post('https://survey-api.herokuapp.com/api/find',
+  .post('https://survey-api.herokuapp.com/api/surveys/find',
    	{ "survey":{
-   		"name": "testCase",
+   		"name": "Android",
    	}},
    	{ json: true },
   	{ headers: { 'Content-Type': 'application/json' }}
    )
-  .expectStatus(201)
-  .expectHeader('Content-Type', 'text/html')
-  .expectHeaderContains('content-type', 'text/html')
+  .afterJSON(function(json) {
+	 console.log("\nTesting the surveys/find endpoint -POST - which uses post data to filter survey results \n1)Expect status -200 \n2)Expect the header to be of type json\n");
+    })
+  .expectStatus(200)
+  .expectHeader('Content-Type', 'application/json')
+  .expectHeaderContains('content-type', 'application/json')
   .toss();
+/*
+  //Testing the /surveys endpoint -POST
+frisby.create('Testing the surveys/find endpoint -POST - which uses post data to create a new survey')
+  .post('https://survey-api.herokuapp.com/api/surveys/',
+   	{ "survey":{
+   		"name": "fromtestingframework",
+   		"subject": "testing",
+   		"country": "IE",
+   		"subscription":{
+   			"frequency":"weekly",
+   			"channel":"email"
+   		}
+   	}},
+   	{ json: true },
+  	{ headers: { 'Content-Type': 'application/json' }}
+   )
+  .expectStatus(200)
+  .expectHeader('Content-Type', 'application/json')
+  .expectHeaderContains('content-type', 'application/json')
+  .toss();*/
 
 //Testing the  /surveys/:survey_id endpoint -DELETE
 /*frisby.create("/surveys/:survey_id endpoint testing - DELETE")
 .delete('https://survey-api.herokuapp.com/api/surveys/56b26e6c1b4148c608f1f216')
 .expectStatus(200)
 .toss();*/
+
+//Testing the error cases
+frisby.create("/api endpoint testing")
+.get('https://survey-api.herokuapp.com/api/Sur')
+.after(function(err, res, body) {
+        console.log("Testing the api basic path");
+    })
+.expectStatus(404)
+.toss();
