2971a19977e811da9f00bcaa26cd9d7645753c3f
Niamh Lawlor <nileahtobhair.github.io>
2016-02-04 01:37:16 +0000

    adding more tests

--- tests/test_spec.js
+++ tests/test_spec.js
@@ -1,9 +1,78 @@
 var frisby = require('frisby');
 
-
-frisby.create("Testing the survery_api")
+//Testing the /surveys endpoint -GET
+frisby.create("/surveys endpoint testing")
 .get('https://survey-api.herokuapp.com/api/surveys')
+.after(function(err, res, body) {
+        console.log("Testing the /surveys endpoint \n1)Expect status -200\n2)Expect 13 surveys to be returned");
+    })
 .expectStatus(200)
-.expectHeaderContains('content-type', 'application/json')
+.expectJSONLength(12)
 .toss();
-//https://survey-api.herokuapp.com/api/surveys
+
+
+//Testing the /surveys/find endpoint - with a single parameter - GET
+frisby.create("/surveys/find endpoint testing - GET")
+.get('https://survey-api.herokuapp.com/api/surveys/find?subject=technology')
+.afterJSON(function(json) {
+
+        console.log("\nTesting the /surveys/find endpoint with subject=technology parameters\n1)Expect status -200 \n2)Expect 5 surveys to be returned");
+    })
+.expectStatus(200)
+.expectJSONLength(5)
+.toss();
+
+//Testing the /surveys/find endpoint - with two parameters - GET
+frisby.create("/surveys/find endpoint testing - GET")
+.get('https://survey-api.herokuapp.com/api/surveys/find?subject=technology&&name=userTesting')
+.afterJSON(function(json) {
+	 console.log("\nTesting the /surveys/find endpoint with two parameters (subject&&name)\n1)Expect status -200 \n2)Expect 1 survey to be returned \n3)Expect JSON to be of right structure, length, content and type");
+    })
+.expectStatus(200)
+.expectJSONLength(1)
+.expectJSONTypes('*', {
+    _id: String,
+    country: String,
+    subject: String,
+    name : String,
+    __v: Number,
+    subscription:{
+    	frequency: String,
+    	channel: String
+    }
+   })
+.toss();
+
+//Testing the /surveys/:survey_id endpoint -GET
+frisby.create("/surveys/:survey_id endpoint testing - GET")
+.get('https://survey-api.herokuapp.com/api/surveys/56b26cd21b4148c608f1f213')
+.afterJSON(function(json) {
+	 console.log("\nTesting the /surveys/:survey_id endpoint \n1)Expect status -200 \n2)Expect the survey to have 6 elements\n3)Expect the correct survey with the correct id to be returned");
+    })
+.expectStatus(200)
+.expectJSONLength(7)
+.expectJSON({
+      _id:"56b26cd21b4148c608f1f213"
+  })
+.toss();
+
+
+//Testing the surveys/find endpoint -POST
+frisby.create('Testing the surveys/find endpoint -POST - which uses post data to filter survey results')
+  .post('https://survey-api.herokuapp.com/api/find',
+   	{ "survey":{
+   		"name": "testCase",
+   	}},
+   	{ json: true },
+  	{ headers: { 'Content-Type': 'application/json' }}
+   )
+  .expectStatus(201)
+  .expectHeader('Content-Type', 'text/html')
+  .expectHeaderContains('content-type', 'text/html')
+  .toss();
+
+//Testing the  /surveys/:survey_id endpoint -DELETE
+/*frisby.create("/surveys/:survey_id endpoint testing - DELETE")
+.delete('https://survey-api.herokuapp.com/api/surveys/56b26e6c1b4148c608f1f216')
+.expectStatus(200)
+.toss();*/
